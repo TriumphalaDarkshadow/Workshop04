f0e91d16c35cb1761630e25455caefdd92207800
TriumphalaDarkshadow <34397221+TriumphalaDarkshadow@users.noreply.github.com>
2019-05-17 12:56:20 +0200

    Update Umsatz.py

--- Umsatz.py
+++ Umsatz.py
@@ -33,9 +33,10 @@ for sheet2 in sheets:
         for anzahl_sheet in wb2:
             sheet_count +=1
             print("Sheet_Count", sheet_count)
+            print(wb2.sheetnames)
         
         
-        #laeuft durch Worksheets
+#        #laeuft durch Worksheets
         for sheet3 in wb2:
             zaehler_dateien += 1
             c7 = sheet['C7'].value
@@ -61,33 +62,7 @@ for sheet2 in sheets:
             print(c_neun)
             print(c_zehn)
             
-#        print("Erstelle neues workbook")
-#        wb3 = Workbook()  
-#        ws = wb3.active
-#        ws.title =str("Umsatz")
-#
-#            #Kunden Namen einlesen
-#        ws['A1'] = 'Es wurden xy Dateien eingelesen'
-#        ws['A3'] = 'Artikel'
-#        ws['A4'] = 'Briefumschlag'
-#        ws['A5'] = 'Bleistift'
-#        ws['A6'] = 'Lineael'
-#        ws['A7'] = 'Textmarker'
-#
-#        ws['B3'] = 'Gesamtzahl'
-#        ws['B4'] = c_sieben
-#        ws['B5'] = c_acht
-#        ws['B6'] = c_neun
-#        ws['B7'] = c_zehn
-#            
-#        print("Es wurden xy Dateien eingelesen")
-#        print("Artikel", "        ", "Gesamtzahl")
-#        print("Briefumschlag", "        ", c_sieben)
-#        print("Bleistift", "        ", c_acht)
-#        print("Lineal", "        ", c_neun)
-#        print("Textmartker", "        ", c_zehn)
-#            
-#        wb3.save('04_Workshop/'+'Umsatz.xlsx')
+
                         
                         
                         #neues excel-File erzeugen
@@ -95,3 +70,30 @@ for sheet2 in sheets:
         print("Datei konnte nicht gefunden werden")
         continue
 
+#        print("Erstelle neues workbook")
+wb3 = Workbook()  
+ws = wb3.active
+ws.title =str("Umsatz")
+
+            #Kunden Namen einlesen
+ws['A1'] = 'Es wurden xy Dateien eingelesen'
+ws['A3'] = 'Artikel'
+ws['A4'] = 'Briefumschlag'
+ws['A5'] = 'Bleistift'
+ws['A6'] = 'Lineael'
+ws['A7'] = 'Textmarker'
+
+ws['B3'] = 'Gesamtzahl'
+ws['B4'] = c_sieben
+ws['B5'] = c_acht
+ws['B6'] = c_neun
+ws['B7'] = c_zehn
+            
+print("Es wurden", sheet_count, "Dateien eingelesen")
+print("Artikel", "        ", "Gesamtzahl")
+print("Briefumschlag", "        ", c_sieben)
+print("Bleistift", "        ", c_acht)
+print("Lineal", "        ", c_neun)
+print("Textmartker", "        ", c_zehn)
+            
+wb3.save('C:\\Users\\alper\\Desktop\\Praktikum\\04_Workshop'+'Umsatz.xlsx')
